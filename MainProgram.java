b92cce779168ee3ff600fc43c168d22ab678c896
Girish-Mehta <girish.mehta100@gmail.com>
2018-02-19 17:02:38 +0530

    Changed query

--- MainProgram.java
+++ MainProgram.java
@@ -3,7 +3,7 @@ class MainProgram{
     public static void main(String[] args){
         DataBase db = new DataBase("ipl.csv");
         QueryParameter queryParameter = new QueryParameter();
-        String query = "Select id,season,city,date,team1,team2 from ipl.csv where season = 2010 and win_by_runs = 140";
+        String query = "Select id,season,city,date,team1,team2 from ipl.csv where win_by_runs = 140";
         queryParameter.setQuery(query);
         db.readFileHeader();
         db.getHeaderType();
