34a60686edb8d4a8b6ff554d7df45637c0868c62
Girish-Mehta <girish.mehta100@gmail.com>
2018-02-19 17:12:12 +0530

    Changed Structure

---
+++ src/MainProgram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class MainProgram{
+    public static void main(String[] args){
+        DataBase db = new DataBase("ipl.csv");
+        QueryParameter queryParameter = new QueryParameter();
+        String query = "Select id,season,city,date,team1,team2 from ipl.csv where win_by_runs = 140";
+        queryParameter.setQuery(query);
+        db.readFileHeader();
+        db.getHeaderType();
+        db.executeQuery(queryParameter);
+    }
+}
