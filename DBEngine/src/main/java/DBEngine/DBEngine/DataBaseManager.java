6ae4bb59055ad994f2f7e772ea1322ddfb6b42fb
Girish-Mehta <girish.mehta100@gmail.com>
2018-02-25 12:41:27 +0530

    Added code for where using >,<,= and order by, group by also for creating JSON output

--- DBEngine/src/main/java/DBEngine/DBEngine/DataBaseManager.java
+++ DBEngine/src/main/java/DBEngine/DBEngine/DataBaseManager.java
@@ -1,61 +1,331 @@
 package DBEngine.DBEngine;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Iterator;
+
 public class DataBaseManager {
 
-	private ArrayList<String> fields = null;
-	private ArrayList<String> row = null;
-	private ArrayList<String> dataType = null;
-
-	public boolean executeQuery(QueryParameter queryParameter){
-
+	private ArrayList<String> data;
+	private ArrayList<String> dataType;
+	private ArrayList<String> fields;
+	private ArrayList<String> header;
+	private ArrayList<String> result;
+	
+	public DataBaseManager() {
+		// TODO Auto-generated constructor stub
+		this.data = new ArrayList<String>();
+		this.dataType = new ArrayList<String>();
+		this.fields = new ArrayList<String>();
+		this.header = new ArrayList<String>();
+		this.result = new ArrayList<String>();
+	}
+	
+	public boolean executeQuery(QueryParameter queryParameter) throws Exception{
+		String[] paramvalue = null;
+		String parameter = "";
+		String value = "";
+		
+		if(queryParameter.getFileName() == null)
+			return false;
+		else if(queryParameter.getFields() == null)
+			return false;
+		
 		try{
 			//LoadFields()
-			LoadFields(queryParameter.getFileName);
+			loadFields(queryParameter.getFileName());
 			//LoadDataTypes()
-			LoadDataTypes();
+			loadDataTypes();
 		} catch(FileNotFoundException e){
-			throw FileNotFoundException;
+			throw e;
 		}
+		
+		// get rows in array list
+		loadData();
+		
+		// group by the result
+		for(String field: queryParameter.getGroups()){
+			sortData(field);
+		}
+				
+		// order by the result
+		for(String field: queryParameter.getOrder()){
+			sortData(field);
+		}
+		
+		// filter result
+		for(String condition: queryParameter.getConditions()) {
+			if(condition.contains("<=")) {
+				paramvalue = condition.split("<=");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, "<=");			
+			} else if(condition.contains(">=")) {
+				paramvalue = condition.split(">=");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, ">=");				
+			} else if(condition.contains("<")) {
+				paramvalue = condition.split("<");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, "<");				
+			} else if(condition.contains(">")) {
+				paramvalue = condition.split(">");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, ">");				
+			} else if(condition.contains("=")) {
+				paramvalue = condition.split("=");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, "=");				
+			} 
+			
+			/* TODO		
+			 else if(condition.toLowerCase().contains("in")) {
+			 	paramvalue = condition.toLowerCase().split("in");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, "=");				
+			 else if(condition.toLowerCase().contains("between")) {
+			 	paramvalue = condition.toLowerCase().split("in");
+				parameter = paramvalue[0]; 
+				value = paramvalue[1];
+				processCondition(parameter, value, "=");				
+			 */
 
-
-		return false;
+			} 
+			
+		/* TODO		
+		// perform function operations
+		for(String function: queryParameter.getFunctions()) {
+			if(function.contains("min")) {
+				
+			}
+		}
+		*/
+		
+		//get required fields
+		filterFields(queryParameter.getFields());
+		
+		// load the output in JSON file
+		try{
+			loadOutput();
+		} catch(Exception e) {
+			throw e;
+		}
+		
+		return true;
 	}
 
-	private void LoadFields(String fileName) {
+	private void loadFields(String fileName) throws FileNotFoundException{
 		// create object
-		DataBaseManager basemanager = new DataBaseManager();
+		FileManager filemanager = new FileManager();
 		try{
 			// load file
-			baseManager.load(fileName);
+			filemanager.load(fileName);
 		} catch(FileNotFoundException e){
-			throw FileNotFoundException;
+			throw e;
 		}
 		// read first row and save column names in list
-		this.fields = baseManager.getFirstRow();
+		this.fields = filemanager.getFirstRow();
 	}
 
-	private void LoadDataTypes() {
+	private void loadDataTypes() {
 		String column = "";
-		String string = "";
-		Integer num = 0;
-		Date date;
 
-		Iterator iterator = fields.Iterator();
+		Iterator<String> iterator = fields.iterator();
 		while(iterator.hasNext()){
 			column = iterator.next();
 			try{
-				date = new date(column);
+				Date date = new Date(column);
 				dataType.add("Date");
 			} catch(Exception e){
 				try{
-					num = (Integer)column;
+					Integer num = (Integer)Integer.parseInt(column);
 					dataType.add("Number");
 				} catch(Exception ee){
-					string = column;
+					String string = column;
 					dataType.add("String");
 				}
 			}
 		}
 	}
 
+	
+	private void loadData() {
+		// create file manager object
+		FileManager filemanager = new FileManager();
+		// get every column in an arrayList
+		this.data = filemanager.getRows();		
+	}
+	
+	private void sortData(String field) {
+		// variable to find location of field
+		int looper = 0;
+		// variable to store location of field 
+		final int pos;
+
+		// iterate every columns and find the right column
+		for(String col: this.fields) {
+			if(field.equalsIgnoreCase(col)) {
+				break;
+			}
+			looper++;
+		}
+		// assign the position of column required
+		pos = looper;
+		
+		// sort the list based upon the value at the location
+		Collections.sort(this.data, new Comparator<String>() {
+
+			@Override
+			public int compare(String r1, String r2) {
+				String[] cols1 = r1.split(",");
+				String[] cols2 = r2.split(",");
+			
+				return cols1[pos].compareToIgnoreCase(cols2[pos]);
+			}
+		});;
+	
+	}
+	
+	private void processCondition(String param, String val, String op) {
+		// variable to store the value to be checked with
+		int value = Integer.parseInt(val);
+		// list to store row location to remove from the result list
+		ArrayList<Integer> locations = new ArrayList<Integer>();
+		// variables to store positions
+		int pos = 0;
+		int loc = 0;
+		
+		// find the position of column to match
+		for(String col: this.fields) {
+			if(col.equalsIgnoreCase(param))
+				break;
+			pos++;
+		}
+		
+		switch(op) {
+		case "<=":
+			// evaluate the condition for required column in every row
+			for(String row: this.data) {
+				String[] cols = row.split(",");
+				// if condition is not met then add the row to the deletion list
+				if(!(Integer.parseInt(cols[pos]) <= value))
+					locations.add(loc);
+				loc++;
+			}
+			break;
+		case ">=":
+			// evaluate the condition for required column in every row
+			for(String row: this.data) {
+				String[] cols = row.split(",");
+				// if condition is not met then add the row to the deletion list
+				if(!(Integer.parseInt(cols[pos]) >= value))
+					locations.add(loc);
+				loc++;
+			}
+			break;
+		case "<":
+			// evaluate the condition for required column in every row
+			for(String row: this.data) {
+				String[] cols = row.split(",");
+				// if condition is not met then add the row to the deletion list
+				if(!(Integer.parseInt(cols[pos]) < value))
+					locations.add(loc);
+				loc++;
+			}
+			break;
+		case ">":			
+			// evaluate the condition for required column in every row
+			for(String row: this.data) {
+				String[] cols = row.split(",");
+				// if condition is not met then add the row to the deletion list
+				if(!(Integer.parseInt(cols[pos]) > value))
+					locations.add(loc);
+				loc++;
+			}
+			break;
+		case "=":
+			// evaluate the condition for required column in every row
+			for(String row: this.data) {
+				String[] cols = row.split(",");
+				// if condition is not met then add the row to the deletion list
+				if(!(cols[pos].equals(val)))
+					locations.add(loc);
+				loc++;
+			}
+			break;
+		default: break;
+		}		
+
+		// remove the rows that did not meet the condition
+		for(int position: locations) {
+			this.data.remove(position);
+		}
+	}
+	
+	private void filterFields(ArrayList<String> columns) {
+		// array list to store the positions required
+		ArrayList<Integer> pos = new ArrayList<Integer>(columns.size());
+		// variable to generate comma separated rows of required fields
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for(String column: columns) {
+			pos.add(this.fields.indexOf(column));
+			this.header.add(column);
+		}
+
+		// for every row
+		for(String row: this.data) {
+			// get columns
+			String[] cols = row.split(",");
+			// for every position required
+			for(Integer loc: pos) {
+				// append the required column
+				stringBuilder.append(cols[loc]+",");
+			}
+			// remove the comma at the end
+			stringBuilder.delete(stringBuilder.length()-1, stringBuilder.length());
+			//save the row in the result array list
+			this.result.add(stringBuilder.toString());
+		}
+	}
+	
+	/* TODO
+	private void processFunction(String column, String op) {
+		switch(op) {
+		case "min":
+			break;
+		case "max":
+			break;
+		case "sum":
+			break;
+		case "avg":
+			break;
+		case "count":
+			break;
+		}
+	}
+	*/
+
+	
+	/*
+	private void loadOutput() throws Exception {
+		// create file manager object
+		FileManager filemanager = new FileManager();
+		try {
+			filemanager.createJSON(this.result, this.header);			
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+	 */
+	
+	// TODO: delete objects and clear memory
 }
--- DBEngine/src/main/java/DBEngine/DBEngine/FileManager.java
+++ DBEngine/src/main/java/DBEngine/DBEngine/FileManager.java
@@ -1,58 +1,148 @@
 package DBEngine.DBEngine;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+
 public class FileManager {
   private BufferedReader bufferedReader;
+  private ArrayList<String> colList;
+  private String fileName;
   private FileReader fileReader;
   private FileWriter fileWriter;
-  private ArrayList<String> colList;
+  private ArrayList<String> rows;
 
   FileManager(){
     this.bufferedReader = null;
+    this.colList = new ArrayList<String>();
+    this.fileName = "";
     this.fileReader = null;
     this.fileWriter = null;
-    this.colList = null;
+    this.rows = new ArrayList<String>();
   }
 
   public void load(String fileName) throws FileNotFoundException{
     try{
       // check if file exists
       fileReader = new FileReader(fileName);
+      this.fileName = fileName;
     } catch(FileNotFoundException e){
       // throw exception if file does not exist
-      throw FileNotFoundException;
+      throw e;
     } finally{
       // close resource
-      fileReader.close();
+      try {
+		fileReader.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
     }
   }
 
-  public ArrayList<String> getFirstRow(){
+  public ArrayList<String> getFirstRow() throws FileNotFoundException{
     String[] columns = null;
     String row = "";
-    int rowCount = 0;
     try{
       // load file
       fileReader = new FileReader(fileName);
       // initialize buffer reader to read lines
       bufferedReader = new BufferedReader(fileReader);
         while((row = bufferedReader.readLine()) != null){
-          // read first row then exit
-          if(rowCount == 1)
-            break;
           // split first line based on comma
           columns = row.split(",");
           for(String column:columns)
             // add every column to list
             colList.add(column);
-          // mark completition of reading row
-          rowCount++;
+          // exit after reading first row
+          break;
         }
       } catch(FileNotFoundException e){
-          throw FileNotFoundException;
-      } finally{
+          throw e;
+      } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} finally{
           // close resource
-          fileReader.close();
+          try {
+			fileReader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
       }
+	return colList;
   }
 
+  
+  public ArrayList<String> getRows(){
+	    String row = "";
+	    try{
+	      // load file
+	      fileReader = new FileReader(fileName);
+	      // initialize buffer reader to read lines
+	      bufferedReader = new BufferedReader(fileReader);
+	        while((row = bufferedReader.readLine()) != null){
+	            // add every row to list
+	            rows.add(row);
+	        }
+	      } catch(FileNotFoundException e){
+	    	  // TODO Auto-generated catch block
+	    	  e.printStackTrace();
+	      } catch (IOException e) {
+	    	  // TODO Auto-generated catch block
+	    	  e.printStackTrace();
+		} finally{
+	          // close resource
+	          try {
+				fileReader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	      }
+		return colList;
+	  }
+  
+  
+//  	public void createJSON(ArrayList<String> result, ArrayList<String> fields) throws Exception{
+//  		int pos = 0;
+//  		// create a json object
+//  		JSONObject obj = new JSONObject();
+//  		// create a json array for every row
+//  		JSONArray rows = new JSONArray();
+//  		// create a json object for every column
+//  		JSONObject row = new JSONObject();
+//
+//  		// for every row in result
+//  		for(String line: result) {
+//  			pos = 0;
+//  			// get every column and put in json
+//  			while(pos < fields.size()) {
+//  	  			row.put(fields.get(pos), result.get(pos));
+//  	  			pos++;
+//  	  		}
+//  			// add that row to json
+//  			rows.put(row);
+//  		}
+//  		// add the rows to json
+//  		obj.put("Output",rows);
+//  		
+//  		try {
+//  			// create json file and write in it
+//  			fileWriter = new FileWriter("output.json");
+//  			fileWriter.write(obj.toJSONString());
+//  	  		fileWriter.flush();
+//  		} catch(IOException e) {
+//  			throw e;
+//  		} finally {
+//  			// close resource
+//  			fileWriter.close();
+//  		}
+//  	}
 }
